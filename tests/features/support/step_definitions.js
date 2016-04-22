aacf741cf7e4c487516b7e19040510e800c0633e
Colin Sagan <colin@quilted.coop>
2016-04-22 19:37:31 +0800

    Add xolvio testing helper meteor packages and start getting the initial step definitions in working order.

--- tests/features/support/step_definitions.js
+++ tests/features/support/step_definitions.js
@@ -1,12 +1,44 @@
+
 module.exports = function() {
-  this.Given(/^I have visited the home page$/, function () {
+  this.Given(/^I am am logged in$/, function () {
+    client.waitForExist('#login-sign-in-link');
+    client.click('#login-sign-in-link');
+    client.click('#signup-link');
+    client.setValue('#login-username', 'letme1n3');
+    client.setValue('#login-password', 'letme1n');
+    client.setValue('#login-password-again', 'letme1n');
+    client.click('#login-buttons-password');
+
+    client.waitForExist('#login-name-link');
+    expect(client.getText('#login-name-link')).toEqual('letme1n3 ▾');
+  });
+
+  this.Given(/^I am an anonymous user$/, function () {
+    client.waitForExist('.login-link-text');
+    expect(client.getText('.login-link-text')).toEqual('Sign in ▾');
+  });
+
+  this.Given(/^I am on the home page$/, function () {
     browser.url('http://localhost:3000');
   });
 
+  this.When(/^I go to the profile page for "([^"]*)"$/, function (name) {
+    // Look up the profile with this name
+    //
+  });
+
   this.Then(/^the "([^"]*)" element should contain "([^"]*)"$/, function (element, text) {
-    browser.waitForText(element);
+    browser.waitForText(element, text);
     const completedText = browser.getTitle();
 
     expect(client.getText(element)).toEqual(text);
   });
+
+  this.Before(function () {
+    // make sure the DDP connection is not logged in before clearing the database
+    server.call('logout');
+    server.execute(function () {
+      Package['xolvio:cleaner'].resetDatabase();
+    });
+  })
 }

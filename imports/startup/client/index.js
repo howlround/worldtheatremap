4492c3f5c0be78a84765ca63d4c4c38ed5f7305c
Colin Sagan <colin@quilted.coop>
2016-08-06 19:34:11 +0800

    Previous attempt at limiting fonts for tests was just disabling them.

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -1,14 +1,11 @@
-// Don't use basic auth locally for tests
-if (Meteor.settings.length > 0) {
-  WebFontConfig = {
-    google: { families: [ 'Oswald:300,400' ] }
-  };
-  (function() {
-    var wf = document.createElement('script');
-    wf.src = '//ajax.googleapis.com/ajax/libs/webfont/1.6.16/webfont.js';
-    wf.type = 'text/javascript';
-    wf.async = 'true';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(wf, s);
-  })();
-}
+WebFontConfig = {
+  google: { families: [ 'Oswald:300,400' ] }
+};
+(function() {
+  var wf = document.createElement('script');
+  wf.src = '//ajax.googleapis.com/ajax/libs/webfont/1.6.16/webfont.js';
+  wf.type = 'text/javascript';
+  wf.async = 'true';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(wf, s);
+})();

46cd6aea049409ec0fbc46e2e03997202de7c43a
Colin Sagan <colin@quilted.coop>
2016-08-06 18:01:48 +0800

    Add Oswald font. Continue moving declarations from original example less to scss.

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -1,11 +1,11 @@
-// WebFontConfig = {
-//   google: { families: [ 'Open+Sans:400,700,700italic,400italic:latin' ] }
-// };
-// (function() {
-//   var wf = document.createElement('script');
-//   wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
-//   wf.type = 'text/javascript';
-//   wf.async = 'true';
-//   var s = document.getElementsByTagName('script')[0];
-//   s.parentNode.insertBefore(wf, s);
-// })();
+WebFontConfig = {
+  google: { families: [ 'Oswald:300,400' ] }
+};
+(function() {
+  var wf = document.createElement('script');
+  wf.src = '//ajax.googleapis.com/ajax/libs/webfont/1.6.16/webfont.js';
+  wf.type = 'text/javascript';
+  wf.async = 'true';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(wf, s);
+})();

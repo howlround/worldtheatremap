4515d66bcaddc14a69e925d426d5cb72b9821719
Colin Sagan <colin@quilted.coop>
2016-08-24 14:41:18 +0800

    Add location and date information to event teasers.

--- imports/ui/components/EventTeaser.jsx
+++ imports/ui/components/EventTeaser.jsx
@@ -9,12 +9,20 @@ export default class EventTeaser extends React.Component {
   render() {
     const { event } = this.props;
 
+    const locationLine = [event.locality, event.administrativeArea, event.country].filter(function (val) {return val;}).join(', ');
+
     return (
       <article className="event-teaser">
         <div className="event-main-info">
           <h3 className="event-name">
             <Link to={`/events/${ event._id }`} key={event._id}>{event.eventType}</Link>
           </h3>
+          { typeof locationLine != 'undefined' ?
+            <div className="event-location">{ locationLine }</div> : '' }
+          { event.dateRange ?
+            <div className="event-date-range date">
+              { event.dateRange }
+            </div> : '' }
         </div>
       </article>
     );
--- tests/features/shows/show_event-listing.feature
+++ tests/features/shows/show_event-listing.feature
@@ -26,6 +26,12 @@ Feature: Events listing on show pages
     And I click on ".DayPicker-Day=1"
     And I click on ".form-group-endDate input"
     And I click on ".DayPicker-Day=15"
+    And I fill in ".event-street-address-edit" with "Brandsen 805"
+    And I fill in ".event-locality-edit" with "Buenos Aires"
+    And I fill in ".event-country-edit" with "Argentina"
+    And I fill in ".event-postal-code-edit" with "1161"
     And I click on ".edit-event-save"
     When I go to the show page for "Sofia"
     Then the "article.event-teaser" element should contain "Performance"
+    And the ".event-location" element should contain "Buenos Aires, Argentina"
+    And the ".event-date-range" element should contain the date range for day "1" to day "15" of this month

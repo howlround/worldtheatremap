5a43c9304dd1fbc77b661c60b1548443b4a5e598
Colin Sagan <colin@quilted.coop>
2016-08-25 13:37:43 +0800

    Clear out Event Add and Edit onChange functions that are not necessary with new autocomplete component.

--- imports/ui/components/EventAdd.jsx
+++ imports/ui/components/EventAdd.jsx
@@ -112,66 +112,6 @@ export default class EventAdd extends React.Component {
 
   onChange(value, path) {
     this.setState(value);
-    // // @TODO: Merge with EventEdit.jsx
-    // if (path[0] == 'show' && path[1] == 'name') {
-    //   const search = value.show.name;
-    //   const resultsElement = $('.form-group-show-name').siblings('ul.autocomplete-results');
-
-    //   // Search for profiles and save to ul.event-show-edit-result
-    //   if (search.length > 0) {
-    //     // Clear any existing stored values
-    //     const clearValue = value;
-    //     clearValue.show.id = '';
-    //     this.setState(clearValue);
-
-    //     const regex = new RegExp('.*' + search + '.*', 'i');
-    //     const results = Shows.find({name: { $regex: regex }}, {limit: 5}).fetch(); // @TODO: Add field limitation
-
-    //     // Clear fields
-    //     resultsElement.html('');
-
-    //     if (results.length > 0) {
-    //       results.map(show => {
-    //         resultsElement.append('<li><b>' + show.name + '</b> (' + show._id + ')</li>').find('li:last-child').click(() => {
-    //             const newValue = value;
-    //             // Set the show state to the selected show
-    //             newValue.show = show;
-    //             // We are using 'id' without the underscore later so
-    //             // manually specify that
-    //             // @TODO: Refactor to only use the _id
-    //             newValue.show.id = show._id;
-
-    //             this.setState(newValue);
-
-    //             // Clear fields
-    //             resultsElement.html('');
-    //         });
-    //       });
-    //     }
-    //     else {
-    //       // New show workflow
-    //       // @TODO: Replace with nested components
-    //       // resultsElement.append('<li>Add Show for <b>' + search + '</b>?</li>').find('li:last-child').click(() => {
-    //       //   // Build a new profile object
-    //       //   const newShow = {
-    //       //     name: search,
-    //       //   }
-    //       //   // Save profile to DB
-    //       //   const newShowObject = this.throttledAddShow(newShow);
-    //       //   // Save the new profile to the new show state
-    //       //   const newValue = value;
-    //       //   newValue.show = newShowObject;
-    //       //   this.setState({show: newValue});
-
-    //       //   // Clear fields
-    //       //   resultsElement.html('');
-    //       // });
-    //     }
-    //   }
-    //   else {
-    //     $('ul.autocomplete-results').html('');
-    //   }
-    // }
   }
 
   render() {
--- imports/ui/components/EventEdit.jsx
+++ imports/ui/components/EventEdit.jsx
@@ -113,50 +113,6 @@ export default class EventEdit extends React.Component {
 
   onChange(value, path) {
     this.setState(value);
-    // @TODO: Merge with EventEdit.jsx
-    if (path[0] == 'show' && path[1] == 'name') {
-      const search = value.show.name;
-      const resultsElement = $('.form-group-show-name').siblings('ul.autocomplete-results');
-
-      // Search for profiles and save to ul.event-show-edit-result
-      if (search.length > 0) {
-        // Clear any existing stored values
-        const clearValue = value;
-        clearValue.show.id = '';
-        this.setState(clearValue);
-
-        const regex = new RegExp('.*' + search + '.*', 'i');
-        const results = Shows.find({name: { $regex: regex }}, {limit: 5}).fetch();
-
-        // Clear fields
-        resultsElement.html('');
-
-        if (results.length > 0) {
-          results.map(show => {
-            resultsElement.append('<li><b>' + show.name + '</b> (' + show._id + ')</li>').find('li:last-child').click(() => {
-                const newValue = value;
-                // Set the show state to the selected show
-                newValue.show = show;
-                // We are using 'id' without the underscore later so
-                // manually specify that
-                // @TODO: Refactor to only use the _id
-                newValue.show.id = show._id;
-
-                this.setState(newValue);
-
-                // Clear fields
-                resultsElement.html('');
-            });
-          });
-        }
-        else {
-          // @TODO: Add new profile workflow
-        }
-      }
-      else {
-        $('ul.autocomplete-results').html('');
-      }
-    }
   }
 
   render() {

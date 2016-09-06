e9192d11186bf68c2f1813542e126dd308c95abb
Colin Sagan <colin@quilted.coop>
2016-09-06 18:00:12 +0800

    Fix events locality filters that broke with react-router array tweaks.

--- imports/ui/containers/SearchEventsResultsContainer.jsx
+++ imports/ui/containers/SearchEventsResultsContainer.jsx
@@ -10,9 +10,18 @@ export default SearchEventsResultsContainer = createContainer((props) => {
   let results = [];
 
   if (!_.isEmpty(query)) {
-    const eventsSubscribe = Meteor.subscribe('events.search', query);
+    // Use an internal query so nothing strange gets passed straight through
+    let privateQuery = {};
+
+    if (query.locality && query.locality instanceof Array) {
+      privateQuery.locality = {
+        $in: query.locality
+      };
+    }
+
+    const eventsSubscribe = Meteor.subscribe('events.search', privateQuery);
     loading = !eventsSubscribe.ready();
-    results = Events.find(query, { sort: { startDate: 1 } }).fetch();
+    results = Events.find(privateQuery, { sort: { startDate: 1 } }).fetch();
   }
 
   return {

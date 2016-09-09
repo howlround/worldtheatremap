f42179111668207eb97858435dbe37312322b229
Colin Sagan <colin@quilted.coop>
2016-09-09 17:28:56 +0800

    Fix lint suggestions for ProfileEdit.jsx.

--- imports/ui/components/ProfileEdit.jsx
+++ imports/ui/components/ProfileEdit.jsx
@@ -1,11 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { _ } from 'meteor/underscore';
 import { displayError } from '../helpers/errors.js';
-import {
-  update,
-  remove,
-} from '../../api/profiles/methods.js';
+import { update } from '../../api/profiles/methods.js';
 import { profileSchema, defaultFormOptions } from '../../api/profiles/profiles.js';
 import t from 'tcomb-form';
 
@@ -37,55 +33,63 @@ export default class ProfileEdit extends React.Component {
     this.onChange = this.onChange.bind(this);
   }
 
-  componentWillReceiveProps() {
-    this.setState(this.props.profile);
-  }
-
   componentDidMount() {
     this.initGoogleMap();
+  }
+
+  componentWillReceiveProps() {
+    this.setState(this.props.profile);
   }
 
   componentDidUpdate() {
     this.initGoogleMap();
   }
 
+  onChange(value) {
+    this.setState(value);
+  }
+
   initGoogleMap() {
     // @TODO: Find a way to unify with ProfileAdd.jsx
     if (GoogleMaps.loaded()) {
-      if ($('.form-group-lat.find-pin-processed').length == 0) {
-        // $('.form-group-lat').hide();
-        // $('.form-group-lon').hide();
-        let initMapLocation = [ 0, 0 ];
+      if ($('.form-group-lat.find-pin-processed').length === 0) {
+        let initMapLocation = [0, 0];
         let initMapZoom = 2;
         if (this.state.lat && this.state.lon) {
-          initMapLocation = [ this.state.lat, this.state.lon ];
+          initMapLocation = [this.state.lat, this.state.lon];
           initMapZoom = 8;
         }
-        // else if (navigator.geolocation) {
-        //     navigator.geolocation.getCurrentPosition(showPosition);
-        // }
-        // function showPosition(position) {
-        //   initMapLocation = [ position.coords.latitude, position.coords.longitude ];
-        // }
 
-        $('<div></div>').addClass('form-group profile-geographic-location-edit').insertBefore('.form-group-lat');
-        $('<div></div>').addClass('find-pin-map').prependTo('.profile-geographic-location-edit').width('100%').height('300px');
-        $('<input></input>').addClass('find-pin').attr({'type': 'text'}).prependTo('.profile-geographic-location-edit').geocomplete({
-          map: ".find-pin-map",
-          details: "form ",
-          detailsAttribute: "data-geo",
-          markerOptions: {
-            draggable: true
-          },
-          mapOptions: {
-            zoom: initMapZoom
-          },
-          location: initMapLocation
-        });
-        $('<label></label>').text('Set Map Pin (optional)').prependTo('.profile-geographic-location-edit');
+        $('<div></div>')
+          .addClass('form-group profile-geographic-location-edit')
+          .insertBefore('.form-group-lat');
+        $('<div></div>')
+          .addClass('find-pin-map')
+          .prependTo('.profile-geographic-location-edit')
+          .width('100%')
+          .height('300px');
+        $('<input></input>')
+          .addClass('find-pin')
+          .attr({ type: 'text' })
+          .prependTo('.profile-geographic-location-edit')
+          .geocomplete({
+            map: '.find-pin-map',
+            details: 'form ',
+            detailsAttribute: 'data-geo',
+            markerOptions: {
+              draggable: true,
+            },
+            mapOptions: {
+              zoom: initMapZoom,
+            },
+            location: initMapLocation,
+          });
+        $('<label></label>')
+          .text('Set Map Pin (optional)')
+          .prependTo('.profile-geographic-location-edit');
 
-        $('.find-pin').bind("geocode:dragged", (event, latLng) => {
-          let updatedProfile = _.extend({}, this.state);
+        $('.find-pin').bind('geocode:dragged', (event, latLng) => {
+          const updatedProfile = _.extend({}, this.state);
           const newLat = latLng.lat();
           const newLon = latLng.lng();
           updatedProfile.lat = newLat.toString();
@@ -93,8 +97,8 @@ export default class ProfileEdit extends React.Component {
           this.setState(updatedProfile);
         });
 
-        $('.find-pin').bind("geocode:result", (event, result) => {
-          let updatedProfile = _.extend({}, this.state);
+        $('.find-pin').bind('geocode:result', (event, result) => {
+          const updatedProfile = _.extend({}, this.state);
           const newLat = result.geometry.location.lat();
           const newLon = result.geometry.location.lng();
           updatedProfile.lat = newLat.toString();
@@ -102,16 +106,12 @@ export default class ProfileEdit extends React.Component {
           this.setState(updatedProfile);
         });
 
-        $('.find-pin').trigger("geocode");
+        $('.find-pin').trigger('geocode');
 
         // Don't process again
         $('.form-group-lat').addClass('find-pin-processed');
       }
     }
-  }
-
-  onChange(value) {
-    this.setState(value);
   }
 
   handleSubmit(event) {
@@ -122,16 +122,15 @@ export default class ProfileEdit extends React.Component {
       this.throttledUpdate(newProfile);
 
       const { router } = this.context;
-      router.push(`/profiles/${ this.props.profile._id }`);
+      router.push(`/profiles/${this.props.profile._id}`);
     }
   }
 
   render() {
-    const { profile } = this.props;
     const formOptions = defaultFormOptions();
 
     return (
-      <form className="profile-edit-form" onSubmit={this.handleSubmit.bind(this)} >
+      <form className="profile-edit-form" onSubmit={this.handleSubmit} >
         <Form
           ref="form"
           type={profileSchema}

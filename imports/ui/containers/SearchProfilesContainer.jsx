893e29cd281f708dd17feeec9b839a592f95f899
Colin Sagan <colin@quilted.coop>
2016-09-13 13:42:27 +0800

    Standardize container declaration in SearchProfilesContainer.

--- imports/ui/containers/SearchProfilesContainer.jsx
+++ imports/ui/containers/SearchProfilesContainer.jsx
@@ -3,7 +3,7 @@ import { TAPi18n } from 'meteor/tap:i18n';
 import { createContainer } from 'meteor/react-meteor-data';
 import SearchProfiles from '../pages/SearchProfiles.jsx';
 
-export default createContainer(() => {
+const SearchProfilesContainer = createContainer(() => {
   const localitiesSubscribe = Meteor.subscribe('localities.public');
   const countriesSubscribe = Meteor.subscribe('countries.public');
   const administrativeAreasSubscribe = Meteor.subscribe('administrativeAreas.public');
@@ -12,3 +12,5 @@ export default createContainer(() => {
     loading: !(localitiesSubscribe.ready() && countriesSubscribe.ready() && administrativeAreasSubscribe.ready() && profilesSubscribe.ready()),
   };
 }, SearchProfiles);
+
+export default SearchProfilesContainer;

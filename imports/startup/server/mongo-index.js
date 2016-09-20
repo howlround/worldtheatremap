cd557e23984f8bd60ef45e4231a510a76e47131e
Colin Sagan <colin@quilted.coop>
2016-09-21 01:45:06 +0800

    Add indexes to Profiles and Shows on the name field.

--- imports/startup/server/mongo-index.js
+++ imports/startup/server/mongo-index.js
@@ -1,5 +1,11 @@
-// import { Profiles } from '../../api/profiles/profiles.js';
+import { Meteor } from 'meteor/meteor';
+import { Profiles } from '../../api/profiles/profiles.js';
+import { Shows } from '../../api/shows/shows.js';
 
-// Profiles._ensureIndex({
-//   "name": "text"
-// });
+Profiles._ensureIndex({
+  "name": 1
+});
+
+Shows._ensureIndex({
+  "name": 1
+});

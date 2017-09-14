840b443cf129236dfc4d82ae83f26e62cccf76fc
Colin Sagan <colin@quilted.coop>
2017-09-14 17:27:46 -0400

    Prefix profile image paths with the user ObjectId to prevent conflicts.

--- imports/startup/server/file-restrictions.js
+++ imports/startup/server/file-restrictions.js
@@ -22,7 +22,7 @@ Meteor.startup( () => {
     key: function (file) {
       //Store file into a directory by the user's username.
       // var user = Meteor.users.findOne(this.userId);
-      return file.name;
+      return `${this.userId}/${file.name}`;
     }
   });
 })

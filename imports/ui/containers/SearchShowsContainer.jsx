8353bcb2789ce8c595230c24baf5767809b74394
Colin Sagan <colin@quilted.coop>
2017-10-09 18:19:55 -0300

    Fix interest filter on Show search.

--- imports/ui/containers/SearchShowsContainer.jsx
+++ imports/ui/containers/SearchShowsContainer.jsx
@@ -1,14 +1,16 @@
 import { Meteor } from 'meteor/meteor';
+import { TAPi18n } from 'meteor/tap:i18n';
 import { createContainer } from 'meteor/react-meteor-data';
 import SearchShows from '../pages/SearchShows.jsx';
 
 export default createContainer(() => {
   const languagesSubscribe = Meteor.subscribe('languages.public');
   const countriesSubscribe = TAPi18n.subscribe('countries.public');
+  const interestsSubscribe = TAPi18n.subscribe('interests.public');
   const localitiesSubscribe = Meteor.subscribe('localities.public');
   const administrativeAreasSubscribe = Meteor.subscribe('administrativeAreas.public');
 
-  const loading = !(localitiesSubscribe.ready() && countriesSubscribe.ready() && administrativeAreasSubscribe.ready() && languagesSubscribe.ready());
+  const loading = !(localitiesSubscribe.ready() && countriesSubscribe.ready() && interestsSubscribe.ready() && administrativeAreasSubscribe.ready() && languagesSubscribe.ready());
   return {
     loading,
   };

0d5e4913eee0bcbf26925f0e3361230557f01db3
Colin Sagan <colin@quilted.coop>
2017-10-10 16:45:46 -0300

    Fix code style of file-restrictions.js.

--- imports/startup/server/file-restrictions.js
+++ imports/startup/server/file-restrictions.js
@@ -1,28 +1,30 @@
-Meteor.startup( () => {
-  Slingshot.fileRestrictions("myFileUploads", {
-    allowedFileTypes: ["image/png", "image/jpeg", "image/jpg", "image/gif"],
-    maxSize: 10 * 1024 * 1024 // 10 MB (use null for unlimited).
+import { Meteor } from 'meteor/meteor';
+import { Slingshot } from 'meteor/edgee:slingshot';
+
+Meteor.startup(() => {
+  Slingshot.fileRestrictions('myFileUploads', {
+    allowedFileTypes: ['image/png', 'image/jpeg', 'image/jpg', 'image/gif'],
+    maxSize: 10 * 1024 * 1024, // 10 MB (use null for unlimited).
   });
 
-  Slingshot.createDirective("myFileUploads", Slingshot.S3Storage, {
+  Slingshot.createDirective('myFileUploads', Slingshot.S3Storage, {
     bucket: Meteor.settings.AWSSourceBucket,
+    acl: 'public-read',
 
-    acl: "public-read",
-
-    authorize: function () {
-      //Deny uploads if user is not logged in.
-      if (!this.userId) {
-        var message = "Please login before posting files";
-        throw new Meteor.Error("Login Required", message);
+    authorize: () => {
+      // Deny uploads if user is not logged in.
+      if (!Meteor.userId()) {
+        const message = 'Please login before posting files';
+        throw new Meteor.Error('Login Required', message);
       }
 
       return true;
     },
 
-    key: function (file) {
-      //Store file into a directory by the user's username.
-      // var user = Meteor.users.findOne(this.userId);
-      return `${this.userId}/${file.name}`;
-    }
+    key: file => (
+      // Store file into a directory by the user's username.
+      // const user = Meteor.users.findOne(this.userId);
+      `${Meteor.userId()}/${file.name}`
+    ),
   });
-})
+});

19b4cb18af352b549cf3e94ae25fb1a31ef74b24
Colin Sagan <colin@quilted.coop>
2017-10-15 19:51:40 -0200

    Code style cleanup on Authors.jsx before using it as model for interests and other search term concatenation.

--- imports/ui/components/Authors.jsx
+++ imports/ui/components/Authors.jsx
@@ -2,40 +2,40 @@ import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import ProfileNameContainer from '../containers/ProfileNameContainer.jsx';
 
-class Authors extends React.Component {
-  render() {
-    const { authors, noLinks } = this.props;
+const Authors = (props) => {
+  const { authors, noLinks } = props;
 
-    // @TODO: Abstract this to a function or component to reduce duplication in EventTeaser.jsx and Event.jsx
-    const authorString = authors.map((author, index, array) => {
-      let seperator = ', ';
-      if (index == array.length - 1) {
-        seperator = '';
-      }
-      else if (index == array.length - 2) {
-        // Had to leave those extra spans to make spaces validate with the react element
-        seperator = (<span><span> </span><FormattedMessage
-          id="show.authorsFinalSeperator"
-          description='When there are multiple authors for a show, the final seperator between names'
-          defaultMessage='and'
-        /><span> </span></span>);
-      }
-
-      return (
-        <span key={author._id}>
-          <ProfileNameContainer profileId={author._id} defaultName={author.name} noLinks={noLinks} />
-          {seperator}
-        </span>
-      );
-    });
+  const authorString = authors.map((author, index, array) => {
+    let seperator = ', ';
+    if (index === array.length - 1) {
+      seperator = '';
+    } else if (index === array.length - 2) {
+      // Had to leave those extra spans to make spaces validate with the react element
+      seperator = (<span><span> </span><FormattedMessage
+        id="show.authorsFinalSeperator"
+        description="When there are multiple authors for a show, the final seperator between names" // eslint-disable-line max-len
+        defaultMessage="and"
+      /><span> </span></span>);
+    }
 
     return (
-      <span>
-        {authorString}
+      <span key={author._id}>
+        <ProfileNameContainer
+          profileId={author._id}
+          defaultName={author.name}
+          noLinks={noLinks}
+        />
+        {seperator}
       </span>
     );
-  }
-}
+  });
+
+  return (
+    <span>
+      {authorString}
+    </span>
+  );
+};
 
 Authors.propTypes = {
   authors: React.PropTypes.array,

60a749753c030cff4d803af827b4211e4b8d7480
Colin Sagan <colin@quilted.coop>
2017-10-23 02:01:23 -0200

    Reformat EventTeaser.jsx as a pure function.

--- imports/ui/components/EventTeaser.jsx
+++ imports/ui/components/EventTeaser.jsx
@@ -3,82 +3,75 @@ import { Link } from 'react-router';
 import { FormattedMessage, FormattedDate, intlShape, injectIntl } from 'react-intl';
 import ProfileNameContainer from '../containers/ProfileNameContainer.jsx';
 
-class EventTeaser extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+const EventTeaser = (props) => {
+  const { event, displayOrg } = props;
+  const { formatMessage, locale } = props.intl;
 
-  render() {
-    const { event, displayOrg } = this.props;
-    const { formatMessage, locale } = this.props.intl;
+  // Can not defineMessages here because countries are coming from the database
+  const locationLine = [
+    event.locality,
+    event.administrativeArea,
+    formatMessage({
+      id: `country.${event.country}`,
+      defaultMessage: event.country,
+      description: `Country options: ${event.country}`,
+    }),
+  ].filter(val => (val)).join(', ');
 
-    // Can not defineMessages here because countries are coming from the database
-    const locationLine = [
-      event.locality,
-      event.administrativeArea,
-      formatMessage({
-        'id': `country.${event.country}`,
-        'defaultMessage': event.country,
-        'description': `Country options: ${event.country}`
-      })
-    ].filter(function (val) {return val;}).join(', ');
-
-    return (
-      <article className="event-teaser">
-        <div className="event-main-info">
-          { (displayOrg && event.organizations) ?
-            <div className="event-organizations">
-              <ProfileNameContainer
-                profileId={event.organizations._id}
-                defaultName={event.organizations.name}
-              />
-            </div>: ''}
-          {typeof locationLine !== 'undefined' ?
-            <div className="event-location">{locationLine}</div> : ''}
-          <div className="event-metadata">
-            <div className="event-type">
-              {
-                formatMessage({
-                  'id': `eventType.${event.eventType}`,
-                  'defaultMessage': event.eventType,
-                  'description': `Interests option: ${event.eventType}`
-                })
-              }
-              <span className="event-type-seperator">:</span>
-            </div>
-            {event.startDate && event.endDate ?
-              <div className="event-date-range date">
-                <FormattedDate
-                  value={event.startDate}
-                  year='numeric'
-                  month='short'
-                  day='numeric'
-                />
-                <span> – </span>
-                <FormattedDate
-                  value={event.endDate}
-                  year='numeric'
-                  month='short'
-                  day='numeric'
-                />
-              </div> : ''}
-          </div>
-          <Link
-            to={`/${locale}/events/${ event._id }`}
-            className="event-view-link"
-            key={event._id}
-          >
-            <FormattedMessage
-              id="eventTeaser.viewEventLink"
-              description="Link directly to event from teaser"
-              defaultMessage="View Event"
+  return (
+    <article className="event-teaser">
+      <div className="event-main-info">
+        {(displayOrg && event.organizations) ?
+          <div className="event-organizations">
+            <ProfileNameContainer
+              profileId={event.organizations._id}
+              defaultName={event.organizations.name}
             />
-          </Link>
+          </div> : ''}
+        {typeof locationLine !== 'undefined' ?
+          <div className="event-location">{locationLine}</div> : ''}
+        <div className="event-metadata">
+          <div className="event-type">
+            {
+              formatMessage({
+                id: `eventType.${event.eventType}`,
+                defaultMessage: event.eventType,
+              })
+            }
+            <span className="event-type-seperator">:</span>
+          </div>
+          {event.startDate && event.endDate ?
+            <div className="event-date-range date">
+              <FormattedDate
+                value={event.startDate}
+                year="numeric"
+                month="short"
+                day="numeric"
+              />
+              <span> – </span>
+              <FormattedDate
+                value={event.endDate}
+                year="numeric"
+                month="short"
+                day="numeric"
+              />
+            </div> : ''}
         </div>
-      </article>
-    );
-  }
-}
+        <Link
+          to={`/${locale}/events/${event._id}`}
+          className="event-view-link"
+          key={event._id}
+        >
+          <FormattedMessage
+            id="eventTeaser.viewEventLink"
+            description="Link directly to event from teaser"
+            defaultMessage="View Event"
+          />
+        </Link>
+      </div>
+    </article>
+  );
+};
 
 EventTeaser.propTypes = {
   event: React.PropTypes.object,

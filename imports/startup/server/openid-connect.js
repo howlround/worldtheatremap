549c9bd769df1fba8fdeee5c1dd642640c94daae
Colin Sagan <colin@quilted.coop>
2018-04-13 16:01:03 -0400

    Cleanup on sso branch. Move oidc info to settings file. Restyle new login link.

--- imports/startup/server/openid-connect.js
+++ imports/startup/server/openid-connect.js
@@ -5,9 +5,9 @@ if (Meteor.isServer) {
       {
         $set: {
           loginStyle: 'redirect',
-          clientId: 'world_theatre_map',
-          secret: 'U2bhXwPfaGzO1VmTgBbMZRfOftc27ahobaixERgHZh5J6t7QQiLTCmVAfqgGxP08',
-          serverUrl: 'http://howlround.local',
+          clientId: Meteor.settings.oidc.clientId,
+          secret: Meteor.settings.oidc.secret,
+          serverUrl: Meteor.settings.oidc.serverUrl,
           authorizationEndpoint: '/oauth2/authorize?',
           tokenEndpoint: '/oauth2/token',
           userinfoEndpoint: '/oauth2/UserInfo',

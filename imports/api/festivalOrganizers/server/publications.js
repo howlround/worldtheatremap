1a7c217eb1d90580648ceba0d1a08c2060795bb4
Colin Sagan <colin@quilted.coop>
2018-04-30 18:54:57 -0400

    Increase the limit for festival and related record publications to 100.

--- imports/api/festivalOrganizers/server/publications.js
+++ imports/api/festivalOrganizers/server/publications.js
@@ -7,14 +7,14 @@ import { FestivalOrganizers } from '../festivalOrganizers.js';
 Meteor.publish('festivalOrganizers.byParent', function festivalOrganizersbyEvent(id) {
   return FestivalOrganizers.find({'parentId': id}, {
     fields: FestivalOrganizers.publicFields,
-    limit: 25,
+    limit: 100,
   });
 });
 
 Meteor.publish('festivalOrganizers.byProfile', function festivalOrganizersbyProfile(id) {
   return FestivalOrganizers.find({'profile._id': id}, {
     fields: FestivalOrganizers.publicFields,
-    limit: 25,
+    limit: 100,
   });
 });
 
@@ -31,7 +31,7 @@ Meteor.publish('festivalOrganizers.anyById', function festivalOrganizersbyProfil
       ],
     }, {
       fields: FestivalOrganizers.publicFields,
-      limit: 25,
+      limit: 100,
     }
   );
 });
